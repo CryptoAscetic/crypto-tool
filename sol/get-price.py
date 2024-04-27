e645933c03715fdf649f868cbcb4679f22b759a9
huangfengge <609232722@qq.com>
2024-04-27 11:41:11 +0800

    feat：新增合约相关的查询方法

--- sol/get-price.py
+++ sol/get-price.py
@@ -3,7 +3,7 @@
 import requests
 
 
-def request_ok(token):
+def get_token_info(token):
     url = f"https://gmgn.ai/defi/quotation/v1/tokens/sol/" + token
     headers = {
         "authority": "gmgn.ai",
@@ -25,29 +25,45 @@ def request_ok(token):
     if response.status_code == 200:
         result = response.json()
         res = result['data']['token']
-        # print(res)
+        print(res)
         arr = []
         price = res['price']
         price_1m = res['price_1m']
         price_5m = res['price_5m']
         price_1h = res['price_1h']
+        holder_count = res['holder_count']
+        quote_reserve = res["pool_info"]["quote_reserve"]
         burn_status = res['burn_status']
         creator_balance = res["creator_balance"]
-        twitter_username = res["social_links"]["twitter_username"]
-        website = res["social_links"]["website"]
-        telegram = res["social_links"]["telegram"]
+        social_links = res["social_links"]
+        if len(social_links) > 0:
+            twitter_username = res["social_links"]["twitter_username"]
+            website = res["social_links"]["website"]
+            telegram = res["social_links"]["telegram"]
+            if twitter_username is None:
+                pass
+            else:
+                arr.append("推特：<" + "https://twitter.com/" + twitter_username + ">\n\r")
+            if website is None:
+                pass
+            else:
+                arr.append("官网地址：<" + website + ">\n\r")
+            if telegram is None:
+                pass
+            else:
+                arr.append("官网地址：<" + telegram + ">\n\r")
         arr.append("当前价格：" + str(price) + "$\n\r")
         arr.append("1分钟前价格：" + str(price_1m) + "$\n\r")
         arr.append("5分钟前价格：" + str(price_5m) + "$\n\r")
         arr.append("1小时前价格：" + str(price_1h) + "$\n\r")
         arr.append("池子是否燃烧：" + burn_status + "\n\r")
         arr.append("合约创建者余额：" + str(creator_balance) + " Sol\n\r")
-        arr.append("推特：<" + "https://twitter.com/" + twitter_username + ">\n\r")
-        arr.append("官网地址：<" + website + ">\n\r")
-        arr.append("官网地址：<" + telegram + ">\n\r")
+        arr.append("合约持有人数：" + str(holder_count) + "\n\r")
+        arr.append("池子sol数：" + str(quote_reserve) + "Sol\n\r")
+
         note_str = "".join(arr)
         print(note_str)
 
 
 if __name__ == '__main__':
-    request_ok("25hAyBQfoDhfWx9ay6rarbgvWGwDdNqcHsXS3jQ3mTDJ")
+    get_token_info("ChanM2vka4gJ3ob1SejJXnxoNuoAXGGJxDMJRSLD7nzW")
