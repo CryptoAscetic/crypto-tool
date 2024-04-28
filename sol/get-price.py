d0745f1567705b7e86da3cd3ce350882518756fb
huangfengge <609232722@qq.com>
2024-04-28 15:44:24 +0800

    feat：新增合约相关的查询方法

--- sol/get-price.py
+++ sol/get-price.py
@@ -36,6 +36,11 @@ def get_token_info(token):
         burn_status = res['burn_status']
         creator_balance = res["creator_balance"]
         social_links = res["social_links"]
+        rug_ratio = res['rug_ratio']
+        holder_rugged_num = res['holder_rugged_num']
+        holder_token_num = res['holder_token_num']
+        hot_level = res['hot_level']
+        burn_ratio = res['burn_ratio']
         if len(social_links) > 0:
             twitter_username = res["social_links"]["twitter_username"]
             website = res["social_links"]["website"]
@@ -57,13 +62,30 @@ def get_token_info(token):
         arr.append("5分钟前价格：" + str(price_5m) + "$\n\r")
         arr.append("1小时前价格：" + str(price_1h) + "$\n\r")
         arr.append("池子是否燃烧：" + burn_status + "\n\r")
+        arr.append("池子燃烧比率：" + str(burn_ratio) + "%\n\r")
         arr.append("合约创建者余额：" + str(creator_balance) + " Sol\n\r")
         arr.append("合约持有人数：" + str(holder_count) + "\n\r")
-        arr.append("池子sol数：" + str(quote_reserve) + "Sol\n\r")
+        arr.append("池子sol数：" + str(quote_reserve) + " Sol\n\r")
+        arr.append("火热等级：" + str(hot_level) + " \n\r")
+        if rug_ratio is None:
+            pass
+        else:
+            arr.append("dev逃跑比例：" + str(rug_ratio) + "%\n\r")
+        if rug_ratio is None:
+            pass
+        else:
+            arr.append("总创建的土狗数：" + str(holder_token_num) + "\n\r")
+        if rug_ratio is None:
+            pass
+        else:
+            arr.append("跑路的土狗数：" + str(holder_rugged_num) + "\n\r")
 
         note_str = "".join(arr)
         print(note_str)
 
 
 if __name__ == '__main__':
-    get_token_info("ChanM2vka4gJ3ob1SejJXnxoNuoAXGGJxDMJRSLD7nzW")
+    # 招财猫
+    # get_token_info("25hAyBQfoDhfWx9ay6rarbgvWGwDdNqcHsXS3jQ3mTDJ")
+
+    get_token_info("CcKGCMM795aVAvYiNpfXLHU4zw6YJbDcUGPRio1uZF26")
