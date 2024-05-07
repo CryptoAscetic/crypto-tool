7c0e6c98029163687405a9d3d073a2f318708bba
huangfengge <609232722@qq.com>
2024-05-07 15:30:50 +0800

    feat：新增class方式进行调整

--- sol/new-token.py
+++ sol/new-token.py
@@ -6,6 +6,8 @@ from datetime import timezone, timedelta, datetime
 
 import requests
 
+from getPrice import GetPrice
+
 token_dd = '2fb4e8566e1348bf837cd8527798b8f4461287a2403bda7d15f9903ee8592909'
 # 分钟
 TIME = 6
@@ -185,8 +187,8 @@ def get_new_token():
 
     }
     response = requests.get(url, headers=headers)
-    arr = []
     print("Status code:", response.status_code)
+    arr = []
     if response.status_code == 200:
         result = response.json()
         res = result['data']['pairs']
@@ -198,173 +200,18 @@ def get_new_token():
             timestamp = int(date.timestamp())
             # 对比的时间8分钟的购买
             diff = 60 * TIME
-            if (timestamp - creation_timestamp) <= diff:
-                arr = get_token_info(token)
-                note_str = "".join(arr)
-                # print(note_str)
-                send_markdown(note_str)
-                time.sleep(5)
-                send_markdown_address(token, "BUY")
-                time.sleep(3)
-
-
-def get_token_info(token):
-    url = f"https://gmgn.ai/defi/quotation/v1/tokens/sol/" + token
-    headers = {
-        "authority": "gmgn.ai",
-        "accept": "application/json, text/plain, */*'",
-        "accept-language": "zh,zh-CN;q=0.9",
-        "cache-control": "no-cache",
-        "cookie": "_ga=GA1.1.1538430465.1713834683; _ga_0XM0LYXGC8=GS1.1.1714184467.2.1.1714184492.0.0.0",
-        "pragma": "no-cache",
-        "referer": "https://gmgn.ai/sol/token/" + token,
-        "sec-ch-ua-platform": "Linux",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 "
-                      "Safari/537.36",
-
-    }
-    response = requests.get(url, headers=headers)
-    arr = []
-    print("Status code:", response.status_code)
-    if response.status_code == 200:
-        result = response.json()
-        res = result['data']['token']
-        print(res)
-        price = res['price']
-        symbol = res['symbol']
-        price_1m = res['price_1m']
-        price_5m = res['price_5m']
-        price_1h = res['price_1h']
-        holder_count = res['holder_count']
-        logo = res['logo']
-        # 检查键'a'是否存在
-        key_to_check = 'pool_info'
-        quote_reserve = "0"
-        burn_status = res['burn_status']
-        if 'creator_balance' in res.keys():
-            creator_balance = res["creator_balance"]
-        else:
-            creator_balance = 0
-        social_links = res["social_links"]
-        rug_ratio = res['rug_ratio']
-        holder_rugged_num = res['holder_rugged_num']
-        holder_token_num = res['holder_token_num']
-        hot_level = res['hot_level']
-        burn_ratio = res['burn_ratio']
-        rugged_tokens = res['rugged_tokens']
-        buys_1m = res['buys_1m']
-        buys_5m = res['buys_5m']
-        sells_1m = res['sells_1m']
-        sells_5m = res['sells_5m']
-        arr.append("![图片地址：](" + logo + ")\n\r")
-        arr.append("名称：" + symbol + "\n\r")
-        renounced_mint = res['renounced_mint']
-        if renounced_mint == 1:
-            arr.append("是否停止mint：" + "是" + "\n\r")
-        else:
-            arr.append("是否停止mint：" + "否" + "\n\r")
-        top_10_holder_rate = res['top_10_holder_rate']
-        arr.append("top10持仓占比：" + str(top_10_holder_rate * 100) + "%\n\r")
-        if len(social_links) > 0:
-            twitter_username = res["social_links"]["twitter_username"]
-            website = res["social_links"]["website"]
-            telegram = res["social_links"]["telegram"]
-            if twitter_username is None:
-                pass
-            else:
-                arr.append("推特：<" + "https://twitter.com/" + twitter_username + ">\n\r")
-            if website is None:
-                pass
-            else:
-                arr.append("官网地址：<" + website + ">\n\r")
-            if telegram is None:
-                pass
-            else:
-                arr.append("电报：<" + telegram + ">\n\r")
-        arr.append("当前价格：" + str(price) + "$\n\r")
-        arr.append("1分钟前价格：" + str(price_1m) + "$\n\r")
-        arr.append("5分钟前价格：" + str(price_5m) + "$\n\r")
-        arr.append("24小时前价格：" + str(price_1h) + "$\n\r")
-        arr.append("1分钟买单次数：" + str(buys_1m) + "次\n\r")
-        arr.append("5分钟买单次数：" + str(buys_5m) + "次\n\r")
-        arr.append("1分钟卖单次数：" + str(sells_1m) + "次\n\r")
-        arr.append("1分钟卖单次数：" + str(sells_5m) + "次\n\r")
-        arr.append("池子是否燃烧：" + burn_status + "\n\r")
-        arr.append("池子燃烧比率：" + str(burn_ratio) + "%\n\r")
-        arr.append("合约创建者余额：" + str(creator_balance) + " Sol\n\r")
-        arr.append("合约持有人数：" + str(holder_count) + "\n\r")
-        arr = get_token_rat(token, arr)
-        arr.append("★火热等级：" + str(hot_level) + " \n\r")
-        if rug_ratio is None:
-            pass
-        else:
-            arr.append("★dev逃跑比例：" + str(rug_ratio * 100) + "%\n\r")
-        if rug_ratio is None:
-            pass
-        else:
-            arr.append("★总创建的土狗数：" + str(holder_token_num) + "\n\r")
-        if rug_ratio is None:
-            pass
-        else:
-            arr.append("★跑路的土狗数：" + str(holder_rugged_num) + "\n\r")
-        # 历史跑路盘
-        if len(rugged_tokens) > 0:
-            for r in rugged_tokens:
-                # arr.append("狗庄的跑路合约：" + str(r['address']) + "\n\r")
-                arr.append("跑路合约名称：" + str(r['symbol']) + "\n\r")
-        if key_to_check in res:
-            quote_reserve = res["pool_info"]["quote_reserve"]
-            arr.append("★当前池子：" + str(quote_reserve) + " Sol\n\r")
-
-        if rug_ratio is None:
-            if float(quote_reserve) > 300.0:
-                if hot_level == 1:
-                    arr.append("【★温馨提示：建议买1s,跑快点★】 \n\r")
-                elif hot_level == 2:
-                    arr.append("【★温馨提示：建议买2s,跑快点★】 \n\r")
-                elif hot_level >= 3:
-                    arr.append("【★温馨提示：建议买3s,跑快点★】 \n\r")
-                else:
-                    arr.append("【★温馨提示，建议先观察★】 \n\r")
-            else:
-                arr.append("【★温馨提示，池子不足300s，小心★】 \n\r")
-        else:
-            arr.append("【★温馨提示，随时跑路，跑快点★】 \n\r")
-
-    return arr
-
-
-def get_token_rat(token, arr):
-    url = f"https://gmgn.ai/defi/quotation/v1/tokens/stats/sol/" + token
-    headers = {
-        "authority": "gmgn.ai",
-        "accept": "application/json, text/plain, */*'",
-        "accept-language": "zh,zh-CN;q=0.9",
-        "cache-control": "no-cache",
-        "cookie": "_ga=GA1.1.1538430465.1713834683; _ga_0XM0LYXGC8=GS1.1.1714184467.2.1.1714184492.0.0.0",
-        "pragma": "no-cache",
-        "referer": "https://gmgn.ai/sol/token/" + token,
-        "sec-ch-ua-platform": "Linux",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 "
-                      "Safari/537.36",
-
-    }
-    response = requests.get(url, headers=headers)
-    print("Status code:", response.status_code)
-    if response.status_code == 200:
-        result = response.json()
-        res = result['data']
-
-        top_rat_trader_count = res['top_rat_trader_count']
-        top_rat_trader_amount_percentage = res['top_rat_trader_amount_percentage']
-
-        arr.append("老鼠仓个数：：" + str(top_rat_trader_count) + "\n\r")
-        arr.append("老鼠仓占比：" + str(top_rat_trader_amount_percentage * 100) + " %\n\r")
-    return arr
+            if not creation_timestamp is None:
+                if (timestamp - creation_timestamp) <= diff:
+                    arr, is_buy = GetPrice.get_token_info(token, arr)
+                    note_str = "".join(arr)
+                    print(note_str)
+                    if is_buy:
+                        # print(note_str)
+                        send_markdown(note_str)
+                        time.sleep(5)
+                        send_markdown_address(token, "BUY")
+                        time.sleep(3)
+                    arr = []
 
 
 if __name__ == '__main__':
