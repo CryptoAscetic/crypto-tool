2520104161fbfdf3d5c92bf617f4b8a11a319269
huangfengge <huang_fengge@163.com>
2024-10-12 22:58:45 +0800

    fix：新增问题解决方式

--- eth/crulTest.py
+++ eth/crulTest.py
@@ -1,33 +1,28 @@
-import subprocess
+import requests
 
-# 定义URL和自定义头信息
-url = "https://gmgn.ai/defi/quotation/v1/wallet_activity/sol?type=buy&type=sell&wallet=82jXFTVu2XwCnG63pGqdf1yAfGMLbmXNzmBE5nupx6YF&limit=10&cost=10"
 headers = {
-    "authority": "gmgn.ai",
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "accept-language": "zh,zh-CN;q=0.9",
-    "cache-control": "no-cache",
-    "cookie": "_ga=GA1.1.1538430465.1713834683; cf_clearance=vdi2icIYeA0pR.pzS47xMbAbBMfnPE5H9_2vIlkFlo4-1728606314-1.2.1.1-eHnu90X7ra2SYI1jS9BhKhZyeTR7YV61_pXpDnCvGbI6SM7.cFMvfSqhC6CIVaXe3UTgO_zdJMtXAi8IaHGy97qOHVnjUuiIRPDXk8nUgUQA9IfrPQVHeOp1K93JwH_waB_YyshoRsJMPpI7eMlNAKE0PbCjfQJ2NfH6vwQyMt9yMHSVBrHw9IEpDuFUMTvT2iq4V5DWqFcHV2vkOV6PXl8tr5i3EBpz2AYHEieSMPKbw5iuyfocP7tLKsBv7xE.hnociI95qWLE4UFDBpaybaAj3vDy7rwt9r4WH_ZoqVfQwNN7B5oKbvq9AKwqVjV7xrKKYmQlvHiQjTrpXy5GH21hiSyHJ_JHSgiUTMCTTXU5DqC2Wx.wIii5_I0bruwjAZtc.lC8I5fkEEERpXsiLk6FjtqDi4BBK1ZsjtI.XPc; _ga_0XM0LYXGC8=GS1.1.1728605831.98.1.1728606400.0.0.0; __cf_bm=OyAVK8mKtlAEmMd5vibrgKHt5VKZWK5wLqRQ1sYl0lA-1728606437-1.0.1.1-FeZE3EpqLQa7Dcrwdg6oJfzCK36ojTfb3GLSMtsIgFxUQde2sBscJHv3SGYsH6h5RNIpTjyhTLI2RlntGy5rZQ",
-    "pragma": "no-cache",
-    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 "
-                  "Safari/537.36",
-    "sec-ch-ua": '"Google Chrome";v="117", "Not;A=Brand";v="8", "Chromium";v="117"',
-    "sec-ch-ua-platform": "Android",
-
+    'authority': 'servapi.dbotx.com',
+    'accept': '*/*',
+    'accept-language': 'zh,zh-CN;q=0.9',
+    'origin': 'https://dbotx.com',
+    'referer': 'https://dbotx.com/',
+    'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Linux"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-site',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    'x-api-key': '',
 }
 
-# 构造curl命令
-curl_command = ["curl", "-s", url]
+params = {
+    'duration': '7',
+    'isWatched': 'false',
+    'page': '0',
+    'size': '24',
+}
 
-# 添加头信息到命令中
-for header, value in headers.items():
-    curl_command.extend(["-H", f"{header}: {value}"])
+response = requests.get('https://servapi.dbotx.com/smart_wallet/assets/statistics', params=params, headers=headers)
 
-try:
-    # 调用curl命令
-    result = subprocess.run(curl_command, capture_output=True, text=True, check=True)
-    # 输出结果
-    print("Response:")
-    print(result.stdout)
-except subprocess.CalledProcessError as e:
-    print(f"Error: {e.stderr}")
+print(response.json())
---
+++ sol/dbotx-smart-token-sol-tg.py
@@ -0,0 +1,238 @@
+# -*- coding: utf-8 -*
+import datetime as dt
+import logging
+import os
+import time
+from datetime import timezone, timedelta, datetime
+
+import coloredlogs
+import mysql.connector
+import requests
+
+from SolTokenPrice import GetSolTokenPrice
+
+# 日志目录
+LOGFILE_FIX = "smart-token-sol-"
+LOG_PATH = os.getcwd() + "/log/"
+logger = logging.getLogger(__name__)
+coloredlogs.install(level='DEBUG', logger=logger)
+# 获取当前日期和时间
+now = datetime.now()
+formatted_date = now.strftime("%Y-%m-%d")
+# 设置日志等级
+logger.setLevel(logging.DEBUG)
+# 追加写入文件a ，设置utf-8编码防止中文写入乱码
+formatted_date_log = logging.FileHandler(LOG_PATH + LOGFILE_FIX + formatted_date + '.log', 'a', encoding='utf-8')
+# 向文件输出的日志级别
+formatted_date_log.setLevel(logging.DEBUG)
+# 向文件输出的日志信息格式
+formatter = logging.Formatter('%(asctime)s - %(filename)s - line:%(lineno)d - %(levelname)s - %(message)s -%(process)s')
+formatted_date_log.setFormatter(formatter)
+# 加载文件到logger对象中
+logger.addHandler(formatted_date_log)
+# 数据库初始化
+mydb = mysql.connector.connect(host='block.chain.com', user='root', password='ute5lU7SrMPfsz', database='blockchain',
+                               port='13306')
+
+TIME = 10
+
+beijing = timezone(timedelta(hours=8))
+print(f'1、北京时区为：{beijing}')
+
+Tokyo = timezone(timedelta(hours=9))
+print(f'2、东京时区为：{Tokyo}')
+
+New_York = timezone(timedelta(hours=-4))
+print(f'3、纽约时区为：{New_York}')
+
+utc = timezone.utc
+print(f'4、世界标准时区为：{utc}')
+
+utc_time = datetime.utcnow()
+print(f'UTC时间为：{utc_time}')
+print(f'本地时间为：{datetime.now()}')
+
+china_time = utc_time.astimezone(beijing)
+time_tokyo = utc_time.astimezone(Tokyo)
+time_newyork = utc_time.astimezone(New_York)
+
+print('1、更改时区为北京后的时间：', china_time)
+print('2、更改时区为东京后的时间：', time_tokyo)
+print('3、更改时区为纽约后的时间：', time_newyork)
+
+
+# 获取当前时间呈现到毫秒级别并转换为时间戳
+def get_current_time_ms_to_timestamp():
+    return int(time.time() * 1000)
+
+
+# 获取当前时间呈现到当天的0时0分0秒000毫秒并转换为时间戳
+def get_current_time_day_to_timestamp():
+    # 获取当日0时0分0秒000毫秒
+    today_0 = dt.datetime.combine(dt.date.today(), dt.time.min)
+    # 转换为时间戳
+    today_0_timestamp = int(time.mktime(today_0.timetuple())) * 1000
+    return today_0_timestamp
+
+
+# 获取当前时间呈现到毫秒级别
+def get_current_time_ms():
+    return dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+
+
+# 获取当前时间呈现到秒级别
+def get_current_time_s():
+    return dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+
+# 将毫秒级别的时间戳转换为时间格式
+def timestamp_to_time(timestamp):
+    return dt.datetime.fromtimestamp(timestamp / 1000).strftime('%Y-%m-%d %H:%M:%S')
+
+
+# 获取当前月份的首日
+def get_current_month_first_day():
+    return dt.datetime.strptime(dt.datetime.now().strftime('%Y-%m') + '-01', '%Y-%m-%d')
+
+
+def send_telegram_photo(photo):
+    token = '7492697040:AAHiTquko-VvkS15tqOcdA5Sk-TLy9EDceQ'
+    chat_id = '-4594318180'
+    url = f'https://api.telegram.org/bot{token}/sendPhoto'
+    payload = {
+        'chat_id': chat_id,
+        'photo': photo,
+        'caption': ''
+    }
+    response = requests.post(url, data=payload)
+    print(response.json())
+
+
+def send_telegram_message(message, tokenAddress):
+    token = '7492697040:AAHiTquko-VvkS15tqOcdA5Sk-TLy9EDceQ'
+    chat_id = '-4594318180'
+    url = f'https://api.telegram.org/bot{token}/sendMessage'
+    inline_keyboard = [
+        [
+            {"text": "✅gmgn", "url": "https://gmgn.ai/sol/token/" + tokenAddress},
+            # {"text": "✅dexlab", "url": "https://www.dexlab.space/mintinglab/spl-token/"
+            #                            "=" + tokenAddress},
+            {"text": "✅buy/sell 一键买卖", "url": "https://t.me/pepeboost_sol04_bot?start"
+                                                  "=" + tokenAddress},
+        ]
+    ]
+    payload = {'chat_id': chat_id, 'text': message, 'parse_mode': 'Markdown',
+               'reply_markup': {
+                   'inline_keyboard': inline_keyboard
+               }, }
+
+    response = requests.post(url, json=payload)
+    print(response.json())
+
+
+def request_ok():
+    tokens = {
+        # '515FRkgdKUunk4BJGndav2FgZniEqYtkdLqcgc8nLSNV': '大帅',
+        '441bsKo6VHuhyUhkDiGxYKXyoZzNuF2Ru4hbPteFiEdn': 'klöss',
+        'DG6QpsjvwqCGyLAYXKEmDBLRfomJ2UAmoe4MJWL9fNtt': 'Queenkayx ❤',
+        'BgJrk3AJEWf41WxThAvDCkZmxuxbt3Q4aZc1P2rrHtZV': 'not BusinessWeek',
+        'Gf9XgdmvNHt8fUTFsWAccNbKeyDXsgJyZN8iFJKg5Pbd': '0xuezhang|985.eth',
+        '4zhALcaaGZQGexZE7VpX5Nk1ihHRDz9nfGCSt3FRy4z1': 'a-z',
+        '4hBL4Z2Tvn2bCNqZniAxL82xviPJaTQeyKMdnLwsVt7L': 'happy第一人',
+        '2pekTQKDsJkd7qUMVD6Z5AGdUuuQ2ZF7zGDmhKjjgVdr': "狙击001"
+    }
+    for token in tokens.keys():
+        time.sleep(3)
+        # 获取所有的数据
+        url = (f"https://servapi.dbotx.com/smart_wallet/trades/" + token)
+        params = {
+            'page': '0',
+            'size': '20',
+            'type': '',
+            'mint': '',
+            'dexName': '',
+        }
+        headers = {
+            'authority': 'servapi.dbotx.com',
+            'accept': '*/*',
+            'accept-language': 'zh,zh-CN;q=0.9',
+            'origin': 'https://dbotx.com',
+            'referer': 'https://dbotx.com/',
+            'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Linux"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-site',
+            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+            'x-api-key': '',
+        }
+        response = requests.get(url, params=params, headers=headers)
+        if response.status_code == 200:
+            result = response.json()
+            res = result['res']
+            arr = []
+            for r in res:  # 第二个实例
+                logger.info(r)
+                # 购买方方式
+                buyType = r["type"]
+                blockTime = r["blockTime"]
+                # 合约名称
+                mint = r["mint"]
+                # 总卖出
+                solAmount = r["solAmount"]
+                # 数量
+                quantity = r["quantity"]
+                usdRate = r["usdRate"]
+                if buyType == "sell":
+                    if 'sellProfit' in r.keys():
+                        # 盈利sol数量
+                        sellProfit = r["sellProfit"]
+                else:
+                    sellProfit = 0.0
+
+                tokenMeta = r["tokenMeta"]
+                if 'updateAt' in tokenMeta.keys():
+                    # 创建时间
+                    createAt = tokenMeta["updateAt"]
+                    timeArray = time.localtime(createAt / 1000)
+                    createAtTime = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
+                else:
+                    createAtTime = "0000-00-00 00:00:00"
+
+                # 合约名称
+                symbol = tokenMeta["symbol"]
+
+                # 获取当前时间
+                date = datetime.now()
+                timestamp = int(date.timestamp())
+                # 对比的时间8分钟的购买
+                diff = 60 * TIME
+                if (timestamp - int(blockTime)) <= diff:
+                    arr.append("聪明钱标签：" + str(tokens[token]) + "\n\r")
+                    arr.append("`合约名称：" + symbol + "`\n\r")
+                    arr.append("`" + mint + "`\n\r")
+                    arr.append("\n\r")
+
+                    arr.append("`💵 交易：`\n\r")
+                    arr.append("|——创建时间：" + createAtTime + "⏰\n\r")
+                    arr.append("|——交易类型：" + buyType + "\n\r")
+                    price = GetSolTokenPrice.get_token_price(mint)
+                    arr.append("|——当前价格：" + format(float(price), '.8f') + " \n\r")
+                    arr.append("\n\r")
+
+                    arr.append("`🔔 量化：`\n\r")
+                    arr.append("|——卖出金额：" + format(float(solAmount), '.2f') + " Sol🟢\n\r")
+                    arr.append("|——卖出数量：" + format(float(quantity), '.2f') + " 个🟢\n\r")
+                    arr.append("|——盈利金额：" + format(float(sellProfit), '.2f') + " Sol🟢\n\r")
+                    arr.append("\n\r")
+
+                    note_str = "".join(arr)
+                    # print(note_str)
+                    logger.info('本次解析的数据：\n\r {0}'.format(note_str))
+                    send_telegram_message(note_str, mint)
+                    arr = []
+
+
+if __name__ == '__main__':
+    request_ok()
