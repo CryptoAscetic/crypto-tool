152d79bbc344869e81622fc8a68f41a58b5c6037
huangfengge <609232722@qq.com>
2024-10-14 11:41:51 +0800

    doc: 频率调整

--- sol/dbotx-smart-token-sol-tg.py
+++ sol/dbotx-smart-token-sol-tg.py
@@ -35,7 +35,7 @@ logger.addHandler(formatted_date_log)
 mydb = mysql.connector.connect(host='block.chain.com', user='root', password='ute5lU7SrMPfsz', database='blockchain',
                                port='13306')
 
-TIME = 20
+TIME = 10
 token_dd = 'be66323915f3254406e75448783a1af708c93ba3ce4d9ec2ebc8bf9e1c5b01dc'
 beijing = timezone(timedelta(hours=8))
 print(f'1、北京时区为：{beijing}')
