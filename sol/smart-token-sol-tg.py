43f64785ae98705c134af9ec22fa69a8b1b930d7
huangfengge <609232722@qq.com>
2024-10-17 20:40:38 +0800

    doc: 新增频率

--- sol/smart-token-sol-tg.py
+++ sol/smart-token-sol-tg.py
@@ -35,8 +35,8 @@ mydb = mysql.connector.connect(host='block.chain.com', user='root', password='ut
                                port='13306')
 
 TIME = 5
-tokenFDVMin = 100000000
-tokenFDVMax = 5000000
+tokenFDVMin = 5000000
+tokenFDVMax = 100000000
 # 5分钟交易额
 tradeVolume5Max = 10000
 
